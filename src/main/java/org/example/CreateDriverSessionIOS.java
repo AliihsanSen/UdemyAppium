ee5409820a2bd3b2c8a65305baa683e2a61abf2d
Ali İhsan Şen <av.aliihsan1@gmail.com>
2023-03-09 18:01:01 +0300

    09.03.2023

--- src/main/java/org/example/CreateDriverSessionIOS.java
+++ src/main/java/org/example/CreateDriverSessionIOS.java
@@ -16,6 +16,7 @@ public class CreateDriverSessionIOS {
         caps.setCapability(MobileCapabilityType.DEVICE_NAME, "iPhone 14");
         caps.setCapability(MobileCapabilityType.AUTOMATION_NAME, "XCUITest");
         caps.setCapability(MobileCapabilityType.UDID, "96D013BE-5B8C-4F7C-AAF6-AA5A4CDB0275");
+        //caps.setCapability("simulatorStartupTimeout",180000);
         caps.setCapability(MobileCapabilityType.APP,
                 "/Users/tribe/Desktop/Projects/MyFirstAppiumProject/src/main/resources/UIKitCatalog-iphonesimulator.app");
 
