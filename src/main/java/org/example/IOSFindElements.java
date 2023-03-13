617c68898e2b00251dd76a9af62e3db3a2988903
Ali İhsan Şen <av.aliihsan1@gmail.com>
2023-03-13 16:27:06 +0300

    13.03.2023

---
+++ src/main/java/org/example/IOSFindElements.java
@@ -0,0 +1,14 @@
+package org.example;
+
+import io.appium.java_client.AppiumDriver;
+
+public class IOSFindElements {
+
+    public static void main(String[] args) throws Exception {
+
+        AppiumDriver driver = CreateDriverSessionOptions.initializeDriver("IOS");
+
+
+
+    }
+}
