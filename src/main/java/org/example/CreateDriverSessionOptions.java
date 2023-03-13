b8a26ee24c0e9780141eb7ad274360d8fdf89f61
Ali İhsan Şen <av.aliihsan1@gmail.com>
2023-03-13 14:03:46 +0300

    13.03.2023

--- src/main/java/org/example/CreateDriverSessionOptions.java
+++ src/main/java/org/example/CreateDriverSessionOptions.java
@@ -1,11 +1,12 @@
 package org.example;
+import io.appium.java_client.remote.MobileCapabilityType;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
 public class CreateDriverSessionOptions {
 
     public static void main(String[] args) {
 
-
-
     }
+
+
 }
