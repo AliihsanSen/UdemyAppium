81b04aa4d66f7ec7724e850567e64a1972f6d729
Ali İhsan Şen <av.aliihsan1@gmail.com>
2023-03-13 14:20:07 +0300

    13.03.2023

--- src/main/java/org/example/AndroidFindElements.java
+++ src/main/java/org/example/AndroidFindElements.java
@@ -2,6 +2,7 @@ package org.example;
 
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.MobileBy;
+import io.appium.java_client.MobileElement;
 
 public class AndroidFindElements {
 
@@ -9,6 +10,9 @@ public class AndroidFindElements {
 
         AppiumDriver driver = CreateDriverSessionOptions.initializeDriver("Android");
 
-        
+        MobileElement myElement = (MobileElement) driver.findElementByAccessibilityId("Accessibility");
+        System.out.println(myElement);
+
+
     }
 }
