93d239292ba158321581129c3527d72879262dec
Ali İhsan Şen <av.aliihsan1@gmail.com>
2023-03-13 14:07:21 +0300

    13.03.2023

--- src/main/java/org/example/CreateDriverSessionOptions.java
+++ src/main/java/org/example/CreateDriverSessionOptions.java
@@ -1,6 +1,10 @@
 package org.example;
+import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.remote.MobileCapabilityType;
 import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class CreateDriverSessionOptions {
 
@@ -8,5 +12,15 @@ public class CreateDriverSessionOptions {
 
     }
 
+    public static AppiumDriver initializeDriver(String platformName) throws MalformedURLException {
 
+        DesiredCapabilities caps = new DesiredCapabilities();
+        caps.setCapability(MobileCapabilityType.PLATFORM_NAME,platformName);
+        caps.setCapability("newCommandTimeıut",300);
+
+        URL url = new URL("http://0.0.0.0:4723/");
+
+
+
+    }
 }
