c28ada257ea4fb39ad5e3d95c89a864cc9e33542
Ali İhsan Şen <av.aliihsan1@gmail.com>
2023-03-13 13:56:15 +0300

    13.03.2023

--- src/main/java/org/example/AndroidFindElements.java
+++ src/main/java/org/example/AndroidFindElements.java
@@ -1,4 +1,12 @@
 package org.example;
 
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.MobileBy;
+
 public class AndroidFindElements {
+
+    public static void main(String[] args) {
+
+        
+    }
 }
