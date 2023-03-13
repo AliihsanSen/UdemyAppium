12aa0afeb2176044e2a9fae819e9e00513729f86
Ali İhsan Şen <av.aliihsan1@gmail.com>
2023-03-13 16:40:47 +0300

    13.03.2023

--- src/main/java/org/example/IOSFindElements.java
+++ src/main/java/org/example/IOSFindElements.java
@@ -1,6 +1,10 @@
 package org.example;
 
 import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.MobileBy;
+import io.appium.java_client.MobileElement;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 
 public class IOSFindElements {
 
@@ -8,7 +12,18 @@ public class IOSFindElements {
 
         AppiumDriver driver = CreateDriverSessionOptions.initializeDriver("IOS");
 
+        driver.findElement(MobileBy.AccessibilityId("Activity Indicators"));
+        WebElement myElement =  driver.findElement(MobileBy.AccessibilityId("Activity Indicators"));
+        System.out.println(myElement.getText());
 
+        myElement = driver.findElement(MobileBy.id("Activity Indicators"));
+        System.out.println(myElement.getText());
+
+        myElement = driver.findElement(MobileBy.className("XCUIElementTypeStaticText"));
+        System.out.println(myElement.getText());
+
+        myElement = driver.findElement(MobileBy.xpath("//XCUIElementTypeStaticText[@name=\"Activity Indicators\"]"));
+        System.out.println(myElement.getText());
 
     }
 }
