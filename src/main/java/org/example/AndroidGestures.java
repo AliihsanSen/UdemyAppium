7e9e2a864d964e16d82dddee85d265bb7db79f1d
Ali İhsan Şen <av.aliihsan1@gmail.com>
2023-03-16 16:18:55 +0300

    16.03.2023

--- src/main/java/org/example/AndroidGestures.java
+++ src/main/java/org/example/AndroidGestures.java
@@ -1,22 +1,27 @@
 package org.example;
 
+import com.google.common.collect.ImmutableMap;
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.MobileBy;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.RemoteWebElement;
 
 public class AndroidGestures {
 
     public static void main(String[] args) throws Exception {
 
-        AppiumDriver driver = CreateDriverSessionOptions.initializeDriver("IOS");
-        driver.findElement(MobileBy.AccessibilityId("Views")).click();
-        driver.findElement(MobileBy.AccessibilityId("Drag and Drop")).click();
+        AppiumDriver driver = CreateDriverSessionOptions.initializeDriver("Android");
+        longClickGesture(driver);
 
     }
 
-    public void longClickGesture(AppiumDriver driver ){
+    public static void longClickGesture(AppiumDriver driver){
 
+        driver.findElement(MobileBy.AccessibilityId("Views")).click();
+        driver.findElement(MobileBy.AccessibilityId("Drag and Drop")).click();
         WebElement element = driver.findElement(MobileBy.id("io.appium.android.apis:id/drag_dot_1"));
+        driver.executeScript("mobile: longClickGesture",
+                ImmutableMap.of("elementId",((RemoteWebElement)element).getId()));
 
 
     }
