0c38f3ac142045e568f047d62028689e461efe64
Ali İhsan Şen <av.aliihsan1@gmail.com>
2023-03-16 14:33:35 +0300

    16.03.2023

--- src/main/java/org/example/FetchElementAttributes.java
+++ src/main/java/org/example/FetchElementAttributes.java
@@ -11,12 +11,12 @@ public class FetchElementAttributes {
         AppiumDriver driver = CreateDriverSessionOptions.initializeDriver("IOS");
 
         By accessibility = MobileBy.AccessibilityId("Activity Indicators");
-        System.out.println("label:" + driver.findElement(accessibility).getText());
-        System.out.println("label:" + driver.findElement(accessibility).getAttribute("label"));
-        System.out.println("value:" + driver.findElement(accessibility).getAttribute("value"));
-        System.out.println("enabled:" + driver.findElement(accessibility).getAttribute("enabled"));
-        System.out.println("selected:" + driver.findElement(accessibility).getAttribute("selected"));
-        System.out.println("visible:" + driver.findElement(accessibility).getAttribute("visible"));
+        System.out.println("label : " + driver.findElement(accessibility).getText());
+        System.out.println("label : " + driver.findElement(accessibility).getAttribute("label"));
+        System.out.println("value : " + driver.findElement(accessibility).getAttribute("value"));
+        System.out.println("enabled : " + driver.findElement(accessibility).getAttribute("enabled"));
+        System.out.println("selected : " + driver.findElement(accessibility).getAttribute("selected"));
+        System.out.println("visible : " + driver.findElement(accessibility).getAttribute("visible"));
 
         System.out.println("selected:" + driver.findElement(accessibility).isSelected());
         System.out.println("enabled:" + driver.findElement(accessibility).isEnabled());
