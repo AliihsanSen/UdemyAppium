c32c35f82eb6e2ee36ad4ce6bff89113507fc710
Ali İhsan Şen <av.aliihsan1@gmail.com>
2023-03-16 16:07:29 +0300

    16.03.2023

--- src/main/java/org/example/AndroidGestures.java
+++ src/main/java/org/example/AndroidGestures.java
@@ -1,12 +1,15 @@
 package org.example;
 
 import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.MobileBy;
 
 public class AndroidGestures {
 
     public static void main(String[] args) throws Exception {
 
         AppiumDriver driver = CreateDriverSessionOptions.initializeDriver("IOS");
+        driver.findElement(MobileBy.AccessibilityId("Views")).click();
+        driver.findElement(MobileBy.AccessibilityId("Drag and Drop")).click();
 
     }
 }
