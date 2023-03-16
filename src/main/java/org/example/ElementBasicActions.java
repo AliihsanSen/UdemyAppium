f22def3dfa8724d6359edfd431bc97a14b384867
Ali İhsan Şen <av.aliihsan1@gmail.com>
2023-03-16 14:26:36 +0300

    16.03.2023

--- src/main/java/org/example/ElementBasicActions.java
+++ src/main/java/org/example/ElementBasicActions.java
@@ -1,4 +1,35 @@
 package org.example;
 
+import com.google.common.collect.ImmutableMap;
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.MobileBy;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.RemoteWebElement;
+
 public class ElementBasicActions {
+
+    public static void main(String[] args) throws Exception {
+        AppiumDriver driver = CreateDriverSessionOptions.initializeDriver("Android");
+
+        By views = MobileBy.AccessibilityId("Views");
+        By textFields = MobileBy.AccessibilityId("TextFields");
+        By editText = MobileBy.id("io.appium.android.apis:id/edit");
+
+        driver.findElement(views).click();
+
+        //Swipe
+        WebElement element = driver.findElement(MobileBy.id("android:id/list"));
+
+        driver.executeScript("mobile: swipeGesture", ImmutableMap.of(
+                "elementId", ((RemoteWebElement) element).getId(),
+                "direction", "up",
+                "percent", 0.75
+        ));
+
+        driver.findElement(textFields).click();
+        driver.findElement(editText).sendKeys("My Text");
+        Thread.sleep(3000);
+        driver.findElement(editText).clear();
+    }
 }
