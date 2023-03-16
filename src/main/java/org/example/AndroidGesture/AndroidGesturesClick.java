052137faba82683d761bac760d0fc880cea73389
Ali İhsan Şen <av.aliihsan1@gmail.com>
2023-03-16 16:35:40 +0300

    16.03.2023

--- src/main/java/org/example/AndroidGesture/AndroidGesturesClick.java
+++ src/main/java/org/example/AndroidGesture/AndroidGesturesClick.java
@@ -1,7 +1,11 @@
 package org.example.AndroidGesture;
 
+import com.google.common.collect.ImmutableMap;
 import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.MobileBy;
 import org.example.CreateDriverSessionOptions;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.RemoteWebElement;
 
 public class AndroidGesturesClick {
 
@@ -9,6 +13,21 @@ public class AndroidGesturesClick {
 
         AppiumDriver driver = CreateDriverSessionOptions.initializeDriver("Android");
 
+    }
+
+    public static void ClickGesture(AppiumDriver driver){
+
+        driver.findElement(MobileBy.AccessibilityId("Views")).click();
+        driver.findElement(MobileBy.AccessibilityId("Drag and Drop")).click();
+        WebElement element = driver.findElement(MobileBy.id("io.appium.android.apis:id/drag_dot_1"));
+        driver.executeScript("mobile: longClickGesture",
+                ImmutableMap.of("elementId",((RemoteWebElement)element).getId(),
+                        "duration",1000));
+
+       /*
+        driver.executeScript("mobile: longClickGesture",
+                ImmutableMap.of("x",218, "y",580,"duration",1000));
+                */
 
     }
 }
