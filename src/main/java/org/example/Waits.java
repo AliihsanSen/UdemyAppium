d3da4e60d1756d10f263605c217582124278db0d
Ali İhsan Şen <av.aliihsan1@gmail.com>
2023-03-16 14:52:34 +0300

    16.03.2023

--- src/main/java/org/example/Waits.java
+++ src/main/java/org/example/Waits.java
@@ -1,9 +1,27 @@
 package org.example;
 
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.MobileBy;
+import org.openqa.selenium.By;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+import java.util.concurrent.TimeUnit;
+
 public class Waits {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
 
+        AppiumDriver driver = CreateDriverSessionOptions.initializeDriver("IOS");
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+        By alertViews = MobileBy.AccessibilityId("Alert Views");
+        By okayCancel = MobileBy.AccessibilityId("Okay / Cancel");
+
+        WebDriverWait wait = new WebDriverWait(driver, 10);
+        wait.until(ExpectedConditions.visibilityOfElementLocated(alertViews)).click();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(okayCancel)).click();
 
     }
 }
