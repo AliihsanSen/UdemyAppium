3d79eb163bb6ebda523a2b81d9805dbeb99d01da
Ali İhsan Şen <av.aliihsan1@gmail.com>
2023-03-16 16:06:00 +0300

    16.03.2023

--- src/main/java/org/example/AndroidGestures.java
+++ src/main/java/org/example/AndroidGestures.java
@@ -1,9 +1,12 @@
 package org.example;
 
+import io.appium.java_client.AppiumDriver;
+
 public class AndroidGestures {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
 
+        AppiumDriver driver = CreateDriverSessionOptions.initializeDriver("IOS");
 
     }
 }
