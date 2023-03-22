391b7af7e132ffc3b19cf0ccc9b3e43c31902fa9
Ali İhsan Şen <av.aliihsan1@gmail.com>
2023-03-22 16:19:28 +0300

    22.03.2023

--- src/main/java/org/example/AndroidGesture/AndroidGesturesDragAndDrop.java
+++ src/main/java/org/example/AndroidGesture/AndroidGesturesDragAndDrop.java
@@ -1,4 +1,18 @@
 package org.example.AndroidGesture;
 
+import io.appium.java_client.AppiumDriver;
+import org.example.CreateDriverSessionOptions;
+
 public class AndroidGesturesDragAndDrop {
+
+    public static void main(String[] args) throws Exception {
+
+        AppiumDriver driver = CreateDriverSessionOptions.initializeDriver("Android");
+
+    }
+
+    public static void longClickGesture(AppiumDriver driver){
+
+
+    }
 }
