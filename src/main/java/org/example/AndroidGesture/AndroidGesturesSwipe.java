fd93b3f9f64a0dbbdbbb57d26abed0c71a1140c8
Ali İhsan Şen <av.aliihsan1@gmail.com>
2023-03-22 17:16:05 +0300

    22.03.2023

--- src/main/java/org/example/AndroidGesture/AndroidGesturesSwipe.java
+++ src/main/java/org/example/AndroidGesture/AndroidGesturesSwipe.java
@@ -1,17 +1,30 @@
 package org.example.AndroidGesture;
 
+import com.google.common.collect.ImmutableMap;
 import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.MobileBy;
 import org.example.CreateDriverSessionOptions;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.RemoteWebElement;
 
 public class AndroidGesturesSwipe {
 
     public static void main(String[] args) throws Exception {
 
         AppiumDriver driver = CreateDriverSessionOptions.initializeDriver("Android");
-
+        swipeGesture(driver);
     }
 
     public static void swipeGesture(AppiumDriver driver){
+
+        driver.findElement(MobileBy.AccessibilityId("Views")).click();
+
+
+        WebElement element = driver.findElement(MobileBy.id("android:id/list"));
+
+        driver.executeScript("mobile: swipeGesture",
+                ImmutableMap.of("elementId",((RemoteWebElement)element).getId(),
+                        "direction","up","percent",0.75));
 
     }
 }
