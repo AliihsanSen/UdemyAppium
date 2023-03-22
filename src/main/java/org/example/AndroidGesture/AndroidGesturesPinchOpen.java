908f1ecfb790de12c819541ee59ec6e595f0c7d5
Ali İhsan Şen <av.aliihsan1@gmail.com>
2023-03-22 16:24:49 +0300

    22.03.2023

---
+++ src/main/java/org/example/AndroidGesture/AndroidGesturesPinchOpen.java
@@ -0,0 +1,17 @@
+package org.example.AndroidGesture;
+
+import io.appium.java_client.AppiumDriver;
+import org.example.CreateDriverSessionOptions;
+
+public class AndroidGesturesPinchOpen {
+
+    public static void main(String[] args) throws Exception {
+
+        AppiumDriver driver = CreateDriverSessionOptions.initializeDriver("Android");
+
+    }
+
+    public static void pinchOpenGesture(AppiumDriver driver){
+
+    }
+}
