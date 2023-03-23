f3279287d05d2fa96ad1bcba9fa9efad67d642e4
Ali İhsan Şen <av.aliihsan1@gmail.com>
2023-03-23 17:13:16 +0300

    23.03.2023

---
+++ src/main/java/org/example/IOSGesture/iOSGesturesTap.java
@@ -0,0 +1,27 @@
+package org.example.IOSGesture;
+
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.MobileBy;
+import org.example.CreateDriverSessionOptions;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.RemoteWebElement;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class iOSGesturesTap {
+    public static void main(String[] args) throws Exception {
+
+        AppiumDriver driver = CreateDriverSessionOptions.initializeDriver("IOS");
+        tap(driver);
+    }
+    public static void tap(AppiumDriver driver){
+        WebElement element = driver.findElement(MobileBy.AccessibilityId("Steppers"));
+
+        Map<String, Object> params = new HashMap<>();
+        params.put("elementId", ((RemoteWebElement) element).getId());
+        params.put("x", 0);
+        params.put("y", 0);
+        driver.executeScript("mobile: tap", params);
+    }
+}
