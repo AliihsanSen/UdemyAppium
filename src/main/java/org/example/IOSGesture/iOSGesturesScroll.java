64fe9dcdb1ece0b58e05fda83fdc56419eb75bbd
Ali İhsan Şen <av.aliihsan1@gmail.com>
2023-03-23 16:37:22 +0300

    23.03.2023

---
+++ src/main/java/org/example/IOSGesture/iOSGesturesScroll.java
@@ -0,0 +1,13 @@
+package org.example.IOSGesture;
+
+import io.appium.java_client.AppiumDriver;
+import org.example.CreateDriverSessionOptions;
+
+public class iOSGesturesScroll {
+
+    public static void main(String[] args) throws Exception {
+
+        AppiumDriver driver = CreateDriverSessionOptions.initializeDriver("IOS");
+    }
+
+}
