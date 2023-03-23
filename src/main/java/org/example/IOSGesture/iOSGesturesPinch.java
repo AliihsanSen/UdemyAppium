670573a8517fa30a63f6864ce369eaef086da38d
Ali İhsan Şen <av.aliihsan1@gmail.com>
2023-03-23 17:00:38 +0300

    23.03.2023

--- src/main/java/org/example/IOSGesture/iOSGesturesPinch.java
+++ src/main/java/org/example/IOSGesture/iOSGesturesPinch.java
@@ -1,4 +1,14 @@
 package org.example.IOSGesture;
 
+import io.appium.java_client.AppiumDriver;
+import org.example.CreateDriverSessionOptions;
+
 public class iOSGesturesPinch {
+
+    public static void main(String[] args) throws Exception {
+
+        AppiumDriver driver = CreateDriverSessionOptions.initializeDriver("IOS");
+
+    }
+
 }
