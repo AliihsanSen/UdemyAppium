fb5fe258e2de731bf1edb8a413e3be30482e91b7
Ali İhsan Şen <av.aliihsan1@gmail.com>
2023-03-23 16:41:34 +0300

    23.03.2023

--- src/main/java/org/example/IOSGesture/iOSGesturesScroll.java
+++ src/main/java/org/example/IOSGesture/iOSGesturesScroll.java
@@ -1,13 +1,39 @@
 package org.example.IOSGesture;
 
 import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.MobileBy;
 import org.example.CreateDriverSessionOptions;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.RemoteWebElement;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class iOSGesturesScroll {
 
     public static void main(String[] args) throws Exception {
 
         AppiumDriver driver = CreateDriverSessionOptions.initializeDriver("IOS");
+        scrollGesture(driver);
     }
+    public static void scrollGesture(AppiumDriver driver) {
+        Map<String, Object> params = new HashMap<>();
+        params.put("direction", "down");
+        driver.executeScript("mobile: scroll", params);
 
+
+        /* WebElement parentElement = driver.findElement(AppiumBy.
+                iOSNsPredicateString("type == \"XCUIElementTypeTable\""));
+                */
+
+        WebElement childElement = driver.findElement(MobileBy.
+                AccessibilityId("Activity Indicators"));
+        params = new HashMap<>();
+        // params.put("direction", "down");
+        params.put("elementId", ((RemoteWebElement) childElement).getId());
+        // params.put("name", "Web View");
+        // params.put("predicateString", "label == \"Web View\"");
+        params.put("toVisible", true);
+        driver.executeScript("mobile: scroll", params);
+    }
 }
