b7c40ac51d5a840e9c7b5d47cd9cd9dc44a8d337
Ali İhsan Şen <av.aliihsan1@gmail.com>
2023-03-23 17:10:03 +0300

    23.03.2023

--- src/main/java/org/example/IOSGesture/iOSGesturesTouchAndHold.java
+++ src/main/java/org/example/IOSGesture/iOSGesturesTouchAndHold.java
@@ -1,7 +1,13 @@
 package org.example.IOSGesture;
 
 import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.MobileBy;
 import org.example.CreateDriverSessionOptions;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.RemoteWebElement;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class iOSGesturesTouchAndHold {
 
@@ -9,5 +15,17 @@ public class iOSGesturesTouchAndHold {
 
         AppiumDriver driver = CreateDriverSessionOptions.initializeDriver("IOS");
 
+    }
+
+    public static void touchAndHold(AppiumDriver driver){
+        driver.findElement(MobileBy.AccessibilityId("Steppers")).click();
+
+        WebElement element = driver.findElement(MobileBy
+                .iOSClassChain("**/XCUIElementTypeButton[`label == \"Increment\"`][1]"));
+
+        Map<String, Object> params = new HashMap<>();
+        params.put("elementId", ((RemoteWebElement) element).getId());
+        params.put("duration", 5);
+        driver.executeScript("mobile: touchAndHold", params);
     }
 }
