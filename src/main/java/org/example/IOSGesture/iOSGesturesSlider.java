08d3ed3b389d91a7ca74f39ffda3520044496817
Ali İhsan Şen <av.aliihsan1@gmail.com>
2023-03-30 15:16:42 +0300

    30.03.2023

--- src/main/java/org/example/IOSGesture/iOSGesturesSlider.java
+++ src/main/java/org/example/IOSGesture/iOSGesturesSlider.java
@@ -1,4 +1,25 @@
 package org.example.IOSGesture;
 
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.MobileBy;
+import org.example.CreateDriverSessionOptions;
+import org.openqa.selenium.WebElement;
+
 public class iOSGesturesSlider {
+    public static void main(String[] args) throws Exception {
+
+        AppiumDriver driver = CreateDriverSessionOptions.initializeDriver("IOS");
+        slider(driver);
+
+    }
+
+    public static void slider(AppiumDriver driver){
+        driver.findElement(MobileBy.AccessibilityId("Sliders")).click();
+
+        WebElement element = driver.findElement(MobileBy.iOSNsPredicateString("value == \"42%\""));
+        element.sendKeys("0");
+
+        element = driver.findElement(MobileBy.iOSNsPredicateString("value == \"0%\""));
+        element.sendKeys("1");
+    }
 }
