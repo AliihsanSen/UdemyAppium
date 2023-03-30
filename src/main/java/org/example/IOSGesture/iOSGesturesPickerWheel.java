61fdf3df4702e1a8ada0db7c2063f72573fc3993
Ali İhsan Şen <av.aliihsan1@gmail.com>
2023-03-30 14:46:19 +0300

    30.03.2023

--- src/main/java/org/example/IOSGesture/iOSGesturesPickerWheel.java
+++ src/main/java/org/example/IOSGesture/iOSGesturesPickerWheel.java
@@ -1,13 +1,65 @@
 package org.example.IOSGesture;
 
 import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.MobileBy;
 import org.example.CreateDriverSessionOptions;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.RemoteWebElement;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class iOSGesturesPickerWheel {
 
     public static void main(String[] args) throws Exception {
 
         AppiumDriver driver = CreateDriverSessionOptions.initializeDriver("IOS");
+
+    }
+    public static void pickerWheel(AppiumDriver driver){
+        driver.findElement(MobileBy.AccessibilityId("Picker View")).click();
+
+        boolean flag = false;
+        while(!flag){
+            WebElement redPickerWheel = driver.findElement(MobileBy.
+                    iOSNsPredicateString("label == \"Red color component value\""));
+            Map<String, Object> params = new HashMap<>();
+            params.put("order", "next");
+            params.put("offset", 0.15);
+            params.put("element", ((RemoteWebElement) redPickerWheel).getId());
+            driver.executeScript("mobile: selectPickerWheelValue", params);
+            if(redPickerWheel.getText().equals("90")){
+                flag = true;
+            }
+        }
+
+        flag = false;
+        while(!flag){
+            WebElement redPickerWheel = driver.findElement(MobileBy.
+                    iOSNsPredicateString("label == \"Green color component value\""));
+            Map<String, Object> params = new HashMap<>();
+            params.put("order", "previous");
+            params.put("offset", 0.15);
+            params.put("element", ((RemoteWebElement) redPickerWheel).getId());
+            driver.executeScript("mobile: selectPickerWheelValue", params);
+            if(redPickerWheel.getText().equals("190")){
+                flag = true;
+            }
+        }
+
+        flag = false;
+        while(!flag){
+            WebElement redPickerWheel = driver.findElement(MobileBy.
+                    iOSNsPredicateString("label == \"Blue color component value\""));
+            Map<String, Object> params = new HashMap<>();
+            params.put("order", "next");
+            params.put("offset", 0.15);
+            params.put("element", ((RemoteWebElement) redPickerWheel).getId());
+            driver.executeScript("mobile: selectPickerWheelValue", params);
+            if(redPickerWheel.getText().equals("135")){
+                flag = true;
+            }
+        }
 
     }
 
