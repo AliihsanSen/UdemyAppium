e504df0f97ee4698e7466d78cfa89a88ad875326
Ali İhsan Şen <av.aliihsan1@gmail.com>
2023-03-30 16:04:00 +0300

    30.03.2023

--- src/main/java/org/example/Android/AndLockAndUnlockDevice.java
+++ src/main/java/org/example/Android/AndLockAndUnlockDevice.java
@@ -1,8 +1,21 @@
 package org.example.Android;
 
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.android.AndroidDriver;
+import org.example.CreateDriverSessionOptions;
+
+import java.util.concurrent.TimeUnit;
+
 public class AndLockAndUnlockDevice {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
+
+        AppiumDriver driver = CreateDriverSessionOptions.initializeDriver("Android");
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+        ((AndroidDriver) driver).lockDevice();
+        System.out.println(((AndroidDriver) driver).isDeviceLocked());
+        ((AndroidDriver) driver).unlockDevice();
 
     }
 }
