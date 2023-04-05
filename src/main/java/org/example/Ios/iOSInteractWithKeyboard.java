a14b56d5111b41333b58f5b6ce9e004625f96292
Ali İhsan Şen <av.aliihsan1@gmail.com>
2023-04-05 16:07:16 +0300

    05.04.2023

--- src/main/java/org/example/Ios/iOSInteractWithKeyboard.java
+++ src/main/java/org/example/Ios/iOSInteractWithKeyboard.java
@@ -1,8 +1,14 @@
 package org.example.Ios;
 
 import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.MobileBy;
+import io.appium.java_client.TouchAction;
+import io.appium.java_client.touch.WaitOptions;
+import io.appium.java_client.touch.offset.PointOption;
 import org.example.CreateDriverSessionOptions;
+import org.openqa.selenium.Dimension;
 
+import java.time.Duration;
 import java.util.concurrent.TimeUnit;
 
 public class iOSInteractWithKeyboard {
@@ -11,5 +17,30 @@ public class iOSInteractWithKeyboard {
 
         AppiumDriver driver = CreateDriverSessionOptions.initializeDriver("IOS");
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+        Dimension size = driver.manage().window().getSize();
+
+        int startX = size.width / 2;
+        int endX = startX;
+        int startY = (int) (size.height*0.8);
+        int endY = (int) (size.height*0.2);
+
+        for( int i =0; i<3;i++){
+            TouchAction touchAction = new TouchAction(driver);
+            touchAction.press(PointOption.point(startX, startY))
+                    .waitAction(WaitOptions.waitOptions(Duration.ofMillis(2000)))
+                    .moveTo(PointOption.point(endX, endY))
+                    .release()
+                    .perform();
+        }
+
+        driver.findElement(MobileBy.AccessibilityId("Text Fields")).click();
+        driver.findElement(MobileBy.xpath("//XCUIElementTypeCell [1]/XCUIElementTypeTextField")).click();
+        Thread.sleep(3000);
+
+        driver.findElement(MobileBy.AccessibilityId("q")).click();
+        driver.findElement(MobileBy.AccessibilityId("w")).click();
+        driver.findElement(MobileBy.AccessibilityId("e")).click();
+        Thread.sleep(3000);
     }
 }
