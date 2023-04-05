eceff85b4030fa754eaf3f81280705e168b8692d
Ali İhsan Şen <av.aliihsan1@gmail.com>
2023-04-05 15:28:16 +0300

    05.04.2023

---
+++ src/main/java/org/example/Ios/iOSInteractsWithApps.java
@@ -0,0 +1,17 @@
+package org.example.Ios;
+
+import io.appium.java_client.AppiumDriver;
+import org.example.CreateDriverSessionOptions;
+
+import java.util.concurrent.TimeUnit;
+
+public class iOSInteractsWithApps {
+
+    public static void main(String[] args) throws Exception {
+
+        AppiumDriver driver = CreateDriverSessionOptions.initializeDriver("IOS");
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+
+    }
+}
