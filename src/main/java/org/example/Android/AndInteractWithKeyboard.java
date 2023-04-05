0231e01ff352d1570a934fd571a6190da8613093
Ali İhsan Şen <av.aliihsan1@gmail.com>
2023-04-05 15:24:51 +0300

    05.04.2023

--- src/main/java/org/example/Android/AndInteractWithKeyboard.java
+++ src/main/java/org/example/Android/AndInteractWithKeyboard.java
@@ -3,15 +3,19 @@ package org.example.Android;
 import com.google.common.collect.ImmutableMap;
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.MobileBy;
+import io.appium.java_client.TouchAction;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.nativekey.AndroidKey;
 import io.appium.java_client.android.nativekey.KeyEvent;
+import io.appium.java_client.touch.WaitOptions;
+import io.appium.java_client.touch.offset.PointOption;
 import org.example.CreateDriverSessionOptions;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.RemoteWebElement;
 
+import java.time.Duration;
 import java.util.concurrent.TimeUnit;
 
 public class AndInteractWithKeyboard {
@@ -34,7 +38,14 @@ public class AndInteractWithKeyboard {
         int startY = (int) (size.height*0.8);
         int endY = (int) (size.height*0.2);
 
-
+        for( int i =0; i<3;i++){
+            TouchAction touchAction = new TouchAction(driver);
+            touchAction.press(PointOption.point(startX, startY))
+                    .waitAction(WaitOptions.waitOptions(Duration.ofMillis(2000)))
+                    .moveTo(PointOption.point(endX, endY))
+                    .release()
+                    .perform();
+        }
 
 
         driver.findElement(textFields).click();
@@ -44,12 +55,9 @@ public class AndInteractWithKeyboard {
         System.out.println(((AndroidDriver) driver).isKeyboardShown());
 
         ((AndroidDriver) driver).pressKey(new KeyEvent().withKey(AndroidKey.C));
-//        driver.getKeyboard().pressKey(Keys.ARROW_DOWN); -> Deprecated
         ((AndroidDriver) driver).pressKey(new KeyEvent().withKey(AndroidKey.HOME));
         ((AndroidDriver) driver).pressKey(new KeyEvent().withKey(AndroidKey.CALENDAR));
-        ((AndroidDriver) driver).pressKey(new KeyEvent().withKey(AndroidKey.B));
         Thread.sleep(3000);
-//        driver.getKeyboard().pressKey(Keys.HOME); -> Deprecated
         ((AndroidDriver) driver).hideKeyboard();
     }
 }
