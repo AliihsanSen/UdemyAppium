0b4ad4b85effebc0679b06b14545e2019119f447
Ali İhsan Şen <av.aliihsan1@gmail.com>
2023-04-05 15:46:38 +0300

    05.04.2023

--- src/main/java/org/example/Ios/iOSInteractsWithApps.java
+++ src/main/java/org/example/Ios/iOSInteractsWithApps.java
@@ -1,8 +1,13 @@
 package org.example.Ios;
 
 import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.MobileBy;
+import io.appium.java_client.MobileElement;
 import org.example.CreateDriverSessionOptions;
+import org.openqa.selenium.By;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
 public class iOSInteractsWithApps {
@@ -12,6 +17,14 @@ public class iOSInteractsWithApps {
         AppiumDriver driver = CreateDriverSessionOptions.initializeDriver("IOS");
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 
+        By activityIndicators = MobileBy.AccessibilityId("Activity Indicators");
+        driver.findElement(activityIndicators).click();
+        Thread.sleep(5000);
+
+        Map<String, Object> params = new HashMap<>();
+        params.put("bundleId", "com.example.apple-samplecode.UICatalog");
+        final boolean isInstalled = (Boolean)driver.executeScript("mobile: isAppInstalled", params);
+        System.out.println(isInstalled);
 
     }
 }
