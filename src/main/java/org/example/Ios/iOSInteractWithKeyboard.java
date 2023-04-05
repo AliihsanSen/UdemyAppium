2073d497bb4adf6ac4b4e22a2fcbb262b2eb93eb
Ali İhsan Şen <av.aliihsan1@gmail.com>
2023-04-05 16:05:13 +0300

    05.04.2023

---
+++ src/main/java/org/example/Ios/iOSInteractWithKeyboard.java
@@ -0,0 +1,15 @@
+package org.example.Ios;
+
+import io.appium.java_client.AppiumDriver;
+import org.example.CreateDriverSessionOptions;
+
+import java.util.concurrent.TimeUnit;
+
+public class iOSInteractWithKeyboard {
+
+    public static void main(String[] args) throws Exception {
+
+        AppiumDriver driver = CreateDriverSessionOptions.initializeDriver("IOS");
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+    }
+}
