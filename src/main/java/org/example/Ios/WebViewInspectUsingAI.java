9c4649f998524a37d3a82159adfa9da1aaa9cc59
Ali İhsan Şen <av.aliihsan1@gmail.com>
2023-04-06 15:34:44 +0300

    06.04.2023

--- src/main/java/org/example/Ios/WebViewInspectUsingAI.java
+++ src/main/java/org/example/Ios/WebViewInspectUsingAI.java
@@ -1,8 +1,13 @@
 package org.example.Ios;
 
 import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.MobileBy;
 import org.example.CreateDriverSessionOptions;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.RemoteWebElement;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
 public class WebViewInspectUsingAI {
@@ -11,6 +16,19 @@ public class WebViewInspectUsingAI {
 
         AppiumDriver driver = CreateDriverSessionOptions.initializeDriver("IOS");
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+        WebElement iOSElement = driver.findElement(MobileBy.
+                iOSNsPredicateString("type == \"XCUIElementTypeTable\""));
+        Map<String, Object> params = new HashMap<>();
+        params.put("direction", "up");
+        params.put("element", ((RemoteWebElement) iOSElement).getId());
+        driver.executeScript("mobile: swipe", params);
+        // -> Scroll code for iOS ends here
+
+        driver.findElement(MobileBy.AccessibilityId("Web View")).click();
+        Thread.sleep(3000);
+        System.out.println(driver.findElement(MobileBy.AccessibilityId("This is HTML content inside a WKWebView ."))
+                .getAttribute("label"));
 
     }
 }
