9b94cccb31926e40df303efee313bd0625bce267
Ali İhsan Şen <av.aliihsan1@gmail.com>
2023-04-07 13:48:45 +0300

    07.04.2023

--- src/main/java/org/example/Android/WebViewAndAutomate.java
+++ src/main/java/org/example/Android/WebViewAndAutomate.java
@@ -1,9 +1,50 @@
 package org.example.Android;
 
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.MobileBy;
+import io.appium.java_client.TouchAction;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.touch.WaitOptions;
+import io.appium.java_client.touch.offset.PointOption;
+import org.example.CreateDriverSessionOptions;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
+
+import java.time.Duration;
+import java.util.Set;
+import java.util.concurrent.TimeUnit;
+
 public class WebViewAndAutomate {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
 
+        AppiumDriver driver = CreateDriverSessionOptions.initializeDriver("Android");
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 
+        driver.findElement(MobileBy.AccessibilityId("Views")).click();
+
+        Dimension size = driver.manage().window().getSize();
+
+        int startX = size.width / 2;
+        int endX = startX;
+        int startY = (int) (size.height*0.8);
+        int endY = (int) (size.height*0.2);
+
+        for( int i =0; i<3;i++){
+            TouchAction touchAction = new TouchAction(driver);
+            touchAction.press(PointOption.point(startX, startY))
+                    .waitAction(WaitOptions.waitOptions(Duration.ofMillis(2000)))
+                    .moveTo(PointOption.point(endX, endY))
+                    .release()
+                    .perform();
+        }
+
+        driver.findElement(MobileBy.AccessibilityId("WebView")).click();
+        Thread.sleep(5000);
+
+        Set<String> contextHandles = ((AndroidDriver<?>) driver).getContextHandles();
+        for(String contextHandle : contextHandles){
+            System.out.println(contextHandle);
+        }
     }
 }
